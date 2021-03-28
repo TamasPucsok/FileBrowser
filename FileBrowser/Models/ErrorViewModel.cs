04eafad868b266732266e92d19a265f1759e4014
TamasPucsok <65065748+TamasPucsok@users.noreply.github.com>
2021-03-28 10:50:52 +0200

    Initial commit

---
+++ FileBrowser/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileBrowser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
